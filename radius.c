5074a5498ef9a7ede0fa30e38e80facb4b28c4d1
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-08-25 21:05:38 +0530

    Radius

---
+++ radius.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    float radius;
+    printf("Enter the radius: ");
+    scanf("%f",&radius);
+    printf("\n The radius is: %f",3.14 * radius * radius);
+    getch();
+}
---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a,b;
+    printf("Enter the calue of a: ");
+    scanf("%d",&a);
+    printf("Enter the calue of b: ");
+    scanf("%d",&b);
+    printf("The sum is = %d",a+b);
+    getch();
+}
