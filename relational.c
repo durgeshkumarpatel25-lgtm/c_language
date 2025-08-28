1cc6bd993ad30fc9cf3758653f6ab31463fc36a6
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-08-28 13:15:37 +0530

    Relational

--- relational.c
+++ relational.c
@@ -3,7 +3,10 @@
 void main()
 {
 int a,b;
-printf("%d",4>5);
+printf("%d\n",4>5);
+printf("%d\n",20>=10 && 10<=50);
+printf("%d\n",50<=100 || 25>50);
+printf("%d\n",!(8>5));
 //Now check the greatest between two number
 printf("\n\n Enter the first number: \n");
 scanf("%d",&a);
