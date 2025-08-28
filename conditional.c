4397c4f7b433550e36c7a76964e782f41d477b2b
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-08-28 14:16:14 +0530

    conditional

---
+++ conditional.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int age;
+    printf("enter your age: \n");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("Adult\t");
+        printf("you are eligible for vote");
+    }
+    else
+    {
+printf("you are not eligible for vote\n");
+    }
+    getch();
+}
