a9be3215378ba5d408d08faf4a3d9342a4f83f87
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-01 22:36:27 +0530

    change

---
+++ factorial.c
@@ -0,0 +1,15 @@
+//program to find the factorial of any number
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n, fact=1;
+    printf("Enter a number do you want factorial \n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    printf("\n the factorial is = %d",fact);
+    getch();
+}
---
+++ odd.c
@@ -0,0 +1,15 @@
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+    int i;
+    printf("All odd numbers from 1 to 50 are\n");
+    for(i=4;i<=50;i++)
+    {
+        if(i%2!=0)
+        {
+        printf("\n%d",i);    //also we can use i++ ans skip if
+        }
+    }
+    getch();
+}
