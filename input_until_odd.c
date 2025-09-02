008135407b8786cc824f93dec7deabddca9dd08c
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-02 20:27:46 +0530

    input_until_odd number

--- input_until_odd.c
+++ input_until_odd.c
@@ -5,7 +5,7 @@ void main()
 int num;
 do
 {
-    printf("Enter number until odd number\n");
+    printf("Enter number until odd number \n");
     scanf("%d",&num);
 } while (num%2==0);
 printf("\n You enterd odd number ");
