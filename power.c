9a7bfc1a8ccbeec0f6e1daf0f97a3ab24f44f15d
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-03 22:06:24 +0530

    power.c

---
+++ power.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    printf("Enter a number for print power: \n");
+    scanf("%d",&a);
+    printf("the power is : %f ",pow(a, 2));
+    return 0;
+}
