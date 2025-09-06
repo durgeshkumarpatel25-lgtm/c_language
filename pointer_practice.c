e876c9a38199439b4f6a90b8fc8208ba5774feb0
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-06 22:47:51 +0530

    pointer

---
+++ pointer_practice.c
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+    int *ptr;
+    int x;
+    ptr =&x;
+    *ptr=0;
+    printf("X= %d \n",x);
+    printf("*ptr = %d \n", *ptr);
+    *ptr +=5;
+    printf("x= %d\n", x);
+    printf("*ptr= %d\n",*ptr);
+    (*ptr)++;
+    printf("x= %d\n",x);
+    printf("*ptr = %d\n", *ptr);
+    return 0;
+}
