4b9374ae81ba04e37452820692a815233e307ca6
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-13 22:06:08 +0530

    fibonacci

--- fibonacci1.c
+++ fibonacci1.c
@@ -1,18 +1,24 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <conio.h>
+
 int main()
 {
     int n;
-    printf("Enter n (n>2): ");
-    scanf("%d",&n);
+    printf("Enter n (n > 2): ");
+    scanf("%d", &n);
+
     int fib[n];
-    fib[0]=0;
-    fib[1]=1;
-    for(int i=2; i<n; i++)
+    fib[0] = 0;
+    fib[1] = 1;
+
+    printf("%d\t%d\t", fib[0], fib[1]); // Pehle do numbers print karein
+
+    for (int i = 2; i < n; i++)
     {
-        fib[i]=fib[i-1] + fib[i-2]; //important
-        printf("%d \t",fib[i]);
+        fib[i] = fib[i - 1] + fib[i - 2]; // important
+        printf("%d\t", fib[i]);
     }
+
     printf("\n");
     return 0;
 }
