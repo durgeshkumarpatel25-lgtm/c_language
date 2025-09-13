4d20f41fa7f290d0af66ae0da83263fc6ad205a4
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-13 22:59:13 +0530

    string.c

---
+++ string.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void printString(char arr[])
+{
+for(int i=0; arr[i]!='\0'; i++)
+{
+    printf("%c", arr[i]);
+}
+printf("\n");
+}
+
+int main()
+{
+    char firstName[]="durgesh";
+    char lastName[]="patel";
+    printString(firstName);
+    printString(lastName);
+    return 0;
+}
