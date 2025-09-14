6c183ddd3e0f13ba275469efe3d0498a438e91e0
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-14 21:45:15 +0530

    string concatination

--- string2.c
+++ string2.c
@@ -1,10 +1,19 @@
-//Example of gets and puts gets means me input from the user and puts means print msg
+//Example of gets and puts gets means me input from the user and puts means print msg and also string lenth
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 void main()
 {
-    char fullName[100];
+     char fullName[100];
     gets(fullName);
     puts(fullName);
+    // for string lenth
+    printf("lenth is:%d ",strlen(fullName));
+    char strFirst[100]="hello";
+    char strSecond[]="world";
+
+    // program for the add two string using concatination
+    strcat(strFirst, strSecond);
+    puts(strFirst);
     getch();
 }
