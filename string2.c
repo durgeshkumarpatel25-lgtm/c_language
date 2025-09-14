bd59fdfac0148c5db9594966c050565082ea1055
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-14 21:31:45 +0530

    string gets and puts

---
+++ string2.c
@@ -0,0 +1,10 @@
+//Example of gets and puts gets means me input from the user and puts means print msg
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char fullName[100];
+    gets(fullName);
+    puts(fullName);
+    getch();
+}
