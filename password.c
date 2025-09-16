4ef3d9813697e9b0ca5d0f5adc99b9ed30061fa6
Durgesh patel <durgeshkumarpatel25@gmail.com>
2025-09-16 22:51:00 +0530

    Password making

---
+++ password.c
@@ -0,0 +1,14 @@
+//enter your password and program will be add own password in your password after your password 123
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    char pass[30];
+    char word[]="123";
+    printf("Enter your password: ");
+    scanf("%s",pass);
+    strcat(pass, word);
+    printf("Final Password Is: %s",pass);
+    getch();
+}
